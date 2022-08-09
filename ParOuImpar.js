7712052ecf8899aac7a9f7cee1b3e76c91c05970
jadeamorim <50496266+jadeamorim@users.noreply.github.com>
2022-08-09 18:20:32 -0300

    Create ParOuImpar.js

---
+++ ParOuImpar.js
@@ -0,0 +1,9 @@
+//Crie um script em JavaScript que dado um número digitado, mostre se ele é Par ou Ímpar.
+
+let numero = Number(prompt("Digite qual número você quer saber se é par ou ímpar"));
+
+if(numero%2==0){
+    alert("O número digitado é par");
+} else {
+    alert("O número digitado é impar");
+}
