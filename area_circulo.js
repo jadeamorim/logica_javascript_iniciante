38e8513d6a004451e643244be226962df712053e
jadeamorim <50496266+jadeamorim@users.noreply.github.com>
2022-09-20 16:59:20 -0300

    Create area_circulo.js

---
+++ area_circulo.js
@@ -0,0 +1,7 @@
+//Faça um programa que peça o raio de um círculo, calcule e mostre sua área.
+
+let raio = Number(prompt("Qual o raio do círculo em cm? "));
+
+let area = Math.PI * raio * raio;
+
+console.log("A área do circulo com de raio", raio, "cm é de", area, "cm².");
